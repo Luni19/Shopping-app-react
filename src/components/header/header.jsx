ac8f55f27eade240d2d8d921ae286d47c54a6bd8
Konstantin <aziimov1999123@gmail.com>
2024-02-18 10:56:50 +0300

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,21 @@
+import './header.css';
+
+function Header () {
+    return(
+        <header>
+            
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </header>
+    )
+    
+}
+
+export default Header;
