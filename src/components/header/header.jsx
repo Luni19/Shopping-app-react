4fb7c8943543459b149177c6b9d1c56b3bebe06e
Konstantin <aziimov1999123@gmail.com>
2024-02-20 20:31:02 +0300

    added header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,30 @@
 import './header.css';
+import logo from '../../images/icons/logo.svg'
 
 function Header () {
     return(
-        <header>
+        <header className='header'>
+            <div className="conteiner">
+                
+                <div className="header-row">
+
+                    <div className="logo"><img src={logo}></img><span>Fashion</span></div>
+                
             
-            <nav>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+            <nav className='navigation'>
+                <ul className='navigation-list'>
+                    <li><a href='#!'>CATALOGUE</a></li>
+                    <li ><a href='#!'>FASHION</a></li>
+                    <li ><a href='#!'>FAVOURITE</a></li>
+                    <li ><a href='#!'>LIFESTYLE</a></li>
+                    <li ><a href='#!'className='sing-up-btn'>SING UP</a></li>
                 </ul>
             </nav>
+            
+            </div>
+
+            </div>
+
         </header>
     )
     
