222dabc1a15668aa2e62d54010c8e7259f72428e
Konstantin <aziimov1999123@gmail.com>
2024-02-22 08:56:59 +0300

    ended hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,11 +7,13 @@ function hero (){
         <div className='conteiner'>
             <div className="hero-content">
                 <div className="hero-text">
-                    <h2 className="hero-tittle"> LET’S EXPLORE UNIQUE CLOTHES.</h2>
+                    <h2 className="hero-tittle"> <span className="word-lets"><span>LET’S</span></span> EXPLORE <span className="word-unique"><span>UNIQUE</span></span> CLOTHES.</h2>
                   <p className="hero-description">
                   Live for Influential and Innovative fashion!
                   </p>
+                  <div className="btn-wrapper">
                   <button type="button" className="hero-btn">Shop Now</button>
+                  </div>
                   </div>
                     <div className="hero-img">
                         <img src={heroImage} alt="heroImage" />  
