cf02faf5fc75e5f1f8ee9577929bd24d44b9e2df
Konstantin <aziimov1999123@gmail.com>
2024-02-28 16:02:10 +0300

    ended footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,34 +1,47 @@
 import "./footer.css"
-
+import fb from "../../images/socials/fb.svg"
+import inst from "../../images/socials/inst.svg"
+import twitter from "../../images/socials/tw.svg"
+import linkin from "../../images/socials/in.svg"
 
 
 function Footer () {
     return (
         <footer>
-        
-         <h3>FASHION</h3>
-         <p>Complete your style with awesome clothes from us.</p>
-
+            <div className="conteiner">
+         <div className="footer-wrapper">
+         <div className="fasion-discription-wrapper">
+         <h3 className="fasion-text">FASHION</h3>
+         <p className="fasion-second-text">Complete your style with awesome clothes from us.</p>
+         <ul className="socials-list">
+            <li><a href=""><img src={fb} alt="" /></a></li>
+            <li><a href=""><img src={inst} alt="" /></a></li>
+            <li><a href=""><img src={twitter} alt="" /></a></li>
+            <li><a href=""><img src={linkin} alt="" /></a></li>
+         </ul>
+         </div>
          <div className="lists-wrapper">
-            <ul>
-                <li>Company</li>
-                <li>About</li>
-                <li>Contact us</li>
-                <li>Support</li>
-                <li>Careers</li>
+            <ul className="contacts-list">
+                <li className="footer-list-item">Company</li>
+                <li className="footer-list-item">About</li>
+                <li className="footer-list-item">Contact us</li>
+                <li className="footer-list-item">Support</li>
+                <li className="footer-list-item">Careers</li>
             </ul>
-            <ul>
-                <li>Quick Link</li>
-                <li>Share Location</li>
-                <li>Orders Tracking</li>
-                <li>Size Guide</li>
-                <li>FAQs</li>
+            <ul className="faq-list">
+                <li className="footer-list-item">Quick Link</li>
+                <li className="footer-list-item">Share Location</li>
+                <li className="footer-list-item">Orders Tracking</li>
+                <li className="footer-list-item">Size Guide</li>
+                <li className="footer-list-item">FAQs</li>
             </ul>
-            <ul>
-                <li>Legal</li>
-                <li>Terms & conditions</li>
-                <li>Privacy Policy</li>
+            <ul className="privacy-list">
+                <li className="footer-list-item">Legal</li>
+                <li className="footer-list-item">Terms & conditions</li>
+                <li className="footer-list-item">Privacy Policy</li>
             </ul>
+         </div>
+         </div>
          </div>
         </footer>
     )
